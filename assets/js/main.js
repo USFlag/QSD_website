434627e16370ecc1e2ad68ac0c37f53d0ad8b6ec
Kenneth Stoddard <info@usflagandbanner.com>
2013-09-12 14:56:07 -0400

    Initial Site Setup.

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+// QSD Master JS Script
+$(document).ready(function(){
+	
+	// Social Button Roll Over
+
+	$("li.facebook a img").on("mouseenter", function () {
+	   $("li.facebook a img").attr( "src", "assets/img/icons/facebook_icon_hover.png" );
+	});
+	$("li.facebook a img").on("mouseleave", function () {
+	   $("li.facebook a img").attr( "src", "assets/img/icons/facebook_icon.png" );
+	});
+	$("li.pintrest a img").on("mouseenter", function () {
+	   $("li.pintrest a img").attr( "src", "assets/img/icons/pintrest_icon_hover.png" );
+	});
+	$("li.pintrest a img").on("mouseleave", function () {
+	   $("li.pintrest a img").attr( "src", "assets/img/icons/pintrest_icon.png" );
+	});
+	
+	
+	// Video Player
+	
+});
