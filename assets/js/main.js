fd9c0adf4edf29a6b28cb56cba7cbe0507abb057
Kenneth Stoddard <info@usflagandbanner.com>
2013-09-12 16:10:58 -0400

    Working video.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,66 @@ $(document).ready(function(){
 	   $("li.pintrest a img").attr( "src", "assets/img/icons/pintrest_icon.png" );
 	});
 	
+	// Close Roll Over
+	$("a.hide-video img").on("mouseenter", function () {
+	   $("a.hide-video img").attr( "src", "assets/img/icons/close_icon.png" );
+	});
+	$("a.hide-video img").on("mouseleave", function () {
+	   $("a.hide-video img").attr( "src", "assets/img/icons/close_icon_hover.png" );
+	});
+	
 	
 	// Video Player
+	
+	var myPlayer = videojs("qsd_video");
+//	$(".explainer-video-btn").on("click", function(){
+//	 	$(".video-js .vjs-tech").attr( "position", "static" );		
+//	 	$(".explainer-video").slideDown(1000);
+//	 	myPlayer.play();
+//	 	/* myPlayer.requestFullScreen(); */
+//	});
+//	$(".hide-video").on("click", function(){
+//	 	myPlayer.pause();
+//	 	$(".explainer-video").hide(3000);
+//	 	/* $(".hide-video").attr( "display", "none" ); */
+//	});
+//	var closeVideo = function (){
+//	   var myPlayer = this;
+//	   $(".explainer-video").hide(3000);
+//	}
+//	myPlayer.on("ended", closeVideo);
+	
+	
+	
+	videojs("qsd_video").ready(function(){
+	
+	  var myPlayer = this;
+	  
+	  
+	  $(".explainer-video-btn").on("click", function(){
+		 	$(".video-js .vjs-tech").attr( "position", "static" );		
+		 	$(".explainer-video").slideDown(1000);
+		 	myPlayer.play();
+		 	/* myPlayer.requestFullScreen(); */
+		});
+		$(".hide-video").on("click", function(){
+		 	myPlayer.pause();
+		 	$(".explainer-video").hide(3000);
+		 	/* $(".hide-video").attr( "display", "none" ); */
+		});
+		var closeVideo = function (){
+		   var myPlayer = this;
+		   $(".explainer-video").hide(3000);
+		}
+		myPlayer.on("ended", closeVideo);
+	   
+	
+	});
+	
+	
+	
+	
+	
+	
 	
 });
