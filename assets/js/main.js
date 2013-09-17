d3dd5459a275bdca8677508737b172c729105c93
Kenneth Stoddard <info@usflagandbanner.com>
2013-09-17 08:19:25 -0400

    Refactored rollover code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,61 +5,80 @@ $(document).ready(function(){
 	// Social Button Roll Over
 
 	$("li.facebook a img").on("mouseenter", function () {
-	   $("li.facebook a img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/facebook_icon_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/facebook_icon_hover.png" );
 	});
 	$("li.facebook a img").on("mouseleave", function () {
-	   $("li.facebook a img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/facebook_icon.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/facebook_icon.png" );
 	});
 	$("li.pintrest a img").on("mouseenter", function () {
-	   $("li.pintrest a img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/pintrest_icon_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/pintrest_icon_hover.png" );
 	});
 	$("li.pintrest a img").on("mouseleave", function () {
-	   $("li.pintrest a img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/pintrest_icon.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/pintrest_icon.png" );
 	});
 	
-	// Close Roll Over
+	// Close Video Roll Over
 	$("a.hide-video img").on("mouseenter", function () {
-	   $("a.hide-video img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/close_icon.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/close_icon.png" );
 	});
 	$("a.hide-video img").on("mouseleave", function () {
-	   $("a.hide-video img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/close_icon_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/icons/close_icon_hover.png" );
 	});
 	
 	// Flag Rollovers
 	$(".flag-chart .feather-dancer-17 img").on("mouseenter", function () {
-	   $(".flag-chart .feather-dancer-17 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_feather_dancer_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_feather_dancer_hover.png" );
 	});
 	$(".flag-chart .feather-dancer-17 img").on("mouseleave", function () {
-	   $(".flag-chart .feather-dancer-17 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_feather_dancer.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_feather_dancer.png" );
 	});
 	
 	$(".flag-chart .feather-dancer-13 img").on("mouseenter", function () {
-	   $(".flag-chart .feather-dancer-13 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_feather_dancer_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_feather_dancer_hover.png" );
 	});
 	$(".flag-chart .feather-dancer-13 img").on("mouseleave", function () {
-	   $(".flag-chart .feather-dancer-13 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_feather_dancer.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_feather_dancer.png" );
 	});
 	
+	// Chart Rollovers
 	$(".flag-chart .kahuna-17 img").on("mouseenter", function () {
-	   $(".flag-chart .kahuna-17 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_kahuna_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_kahuna_hover.png" );
 	});
 	$(".flag-chart .kahuna-17 img").on("mouseleave", function () {
-	   $(".flag-chart .kahuna-17 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_kahuna.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/17_kahuna.png" );
 	});
 	
 	$(".flag-chart .kahuna-13 img").on("mouseenter", function () {
-	   $(".flag-chart .kahuna-13 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_kahuna_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_kahuna_hover.png" );
 	});
 	$(".flag-chart .kahuna-13 img").on("mouseleave", function () {
-	   $(".flag-chart .kahuna-13 img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_kahuna.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/13_kahuna.png" );
 	});
 	
 	$(".flag-chart .horizontal img").on("mouseenter", function () {
-	   $(".flag-chart .horizontal img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/horizontal_flag_hover.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/horizontal_flag_hover.png" );
 	});
 	$(".flag-chart .horizontal img").on("mouseleave", function () {
-	   $(".flag-chart .horizontal img").attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/horizontal_flag.png" );
+	   $(this).attr( "src", "http://www.qualitysigndesigner.com/assets/img/buttons/horizontal_flag.png" );
 	});
+	
+	
+	// Video CTA BTN Rollovers
+	$(".start-designing-now a img").on("mouseenter", function () {
+	   $(this).attr( "src", "http://qualitysigndesigner.com/assets/img/buttons/start_designing_now_btn_hover.png" );
+	});
+	$(".start-designing-now a img").on("mouseleave", function () {
+	   $(this).attr( "src", "http://qualitysigndesigner.com/assets/img/buttons/start_designing_now_btn.png" );
+	});
+	
+	$(".explainer-video-btn a img").on("mouseenter", function () {
+	   $(this).attr( "src", "http://qualitysigndesigner.com/assets/img/buttons/explainer_video_btn_hover.png" );
+	});
+	$(".explainer-video-btn a img").on("mouseleave", function () {
+	   $(this).attr( "src", "http://qualitysigndesigner.com/assets/img/buttons/explainer_video_btn.png" );
+	});
+	
+	
 	
 	
 	// Video Player
@@ -81,7 +100,7 @@ $(document).ready(function(){
 			 	var howMuchIsDownloaded = myPlayer.bufferedPercent();
 			 	//console.log(howMuchIsDownloaded);
 			 	if(howMuchIsDownloaded > 0.25){
-				 	//myPlayer.pause();
+				 	myPlayer.pause();
 			 	}
 		 	}
 		 	
