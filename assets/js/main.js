9d35ba9fa41a9606190d9e88dca5b9e7a7041203
Kenneth Stoddard <info@usflagandbanner.com>
2013-10-04 16:04:16 -0400

    Vimeo Integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,49 @@ $(document).ready(function(){
 	});
 	
 	
-	// Video CTA BTN Rollovers
+	// Video Player Show/Hide
+	$(".explainer-video-btn").on("click", function(){	
+		 $("#qsd-video").slideDown(1000);
+	});
+		
+	$(".hide-video").on("click", function(){
+		 $("#qsd-video").hide(3000);
+	});
+	
+	// Vimeo Player API
+	var iframe = $('#player1')[0],
+    player = $f(iframe),
+    status = $('.status');
+
+		// When the player is ready, add listeners for pause, finish, and playProgress
+		player.addEvent('ready', function() {
+		    status.text('ready');
+		    
+		    player.addEvent('pause', onPause);
+		    player.addEvent('finish', onFinish);
+		    player.addEvent('playProgress', onPlayProgress);
+		});
+		
+		// Call the API when a button is pressed
+		$('a.hide-video img').bind('click', function() {
+		    player.api("pause");
+		});
+		
+		function onPause(id) {
+		    status.text('paused');
+		}
+		
+		function onFinish(id) {
+		    status.text('finished');
+		    $("#qsd-video").hide(3000);
+		}
+		
+		function onPlayProgress(data, id) {
+		    status.text(data.seconds + 's played');
+		}
+	
+	
+	// Video and Desing Now CTA BTN Rollovers
 	$(".start-designing-now a img").on("mouseenter", function () {
 	   $(this).attr( "src", "http://qualitysigndesigner.com/assets/img/buttons/start_designing_now_btn_hover.png" );
 	});
